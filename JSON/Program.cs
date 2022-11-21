0c68c6e939f5772ec6ee1fb25e4ac1089d208539
pato6094 <111519569+pato6094@users.noreply.github.com>
2022-11-21 10:13:29 +0100

    Aggiungere i file di progetto.

---
+++ JSON/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        }
+
+    }
+}
